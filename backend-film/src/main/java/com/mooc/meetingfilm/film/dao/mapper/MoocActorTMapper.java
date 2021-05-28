a3a6e563fe9a95eb6ece434f68c64ee68a2d3499
cary <598539403@qq.com>
2021-05-29 01:42:52 +0800

    增加backend_film模块,通过MP自动生成DAO实体类和mapper

---
+++ backend-film/src/main/java/com/mooc/meetingfilm/film/dao/mapper/MoocActorTMapper.java
@@ -0,0 +1,16 @@
+package com.mooc.meetingfilm.film.dao.mapper;
+
+import com.mooc.meetingfilm.film.dao.entity.MoocActorT;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 演员表 Mapper 接口
+ * </p>
+ *
+ * @author caryguan
+ * @since 2021-05-29
+ */
+public interface MoocActorTMapper extends BaseMapper<MoocActorT> {
+
+}
---
+++ backend-film/src/main/java/com/mooc/meetingfilm/film/dao/mapper/MoocFilmTMapper.java
@@ -0,0 +1,16 @@
+package com.mooc.meetingfilm.film.dao.mapper;
+
+import com.mooc.meetingfilm.film.dao.entity.MoocFilmT;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 影片主表 Mapper 接口
+ * </p>
+ *
+ * @author caryguan
+ * @since 2021-05-29
+ */
+public interface MoocFilmTMapper extends BaseMapper<MoocFilmT> {
+
+}
